14fb6260d04e31053eb3864c5d96cd66b37d4e57
thom <thomas.ivantoro@naradacode.com>
2020-05-19 15:39:43 +0700

    init repo

---
+++ Test Suites/TS - Login Admin.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS - Login Admin</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>4d1b9fed-e76b-4aab-9e86-9f94a78f1e97</testSuiteGuid>
+   <testCaseLink>
+      <guid>6f299568-4975-4d76-be8a-a9ddb40c143b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User Security/TC - Login Logout</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
